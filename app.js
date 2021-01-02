8c8e5e8c1143ba945d14a2de330c3c6272fe3baf
Moeen-Ul-Islam <moeenulislam786@gmail.com>
2021-01-02 17:13:12 +0530

    first commit- created required files to work on

---
+++ app.js
@@ -0,0 +1,5 @@
+// http://www.omdbapi.com/?i=tt3896198&apikey=4e1c96a5;
+//http://www.omdbapi.com/?apikey=[yourkey]&
+const APIKEY = 4e1c96a5;
+const APIURL = http://www.omdbapi.com/?i=tt3896198&apikey=;
+console.log(api);
